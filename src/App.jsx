532efade930c402c4b973ce7b17f0bc29235d328
vkmarisetty <63303823+royVarinder@users.noreply.github.com>
2026-08-01 13:33:30 +0530

    new version of portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { About, Contact, Experience, Footer, Header, Hero, Projects, Skills } from './Components/Elements'
+import { NewHeader, NewHero, NewExperience, NewTechOrbit, NewProjects, NewContact, NewFooter } from './Components/NewElements'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return <>
     <div className='align-left'>
-      <Header />
-      <Hero />
-      {/* <About /> */}
-      <Projects />
-      <Experience />
-      <Skills />
-      {/* <Contact /> */}
-      <Footer />
+      <NewHeader />
+      <NewHero />
+      <NewExperience />
+      <NewTechOrbit />
+      <NewProjects />
+      <NewContact />
+      <NewFooter />
     </div>
-
   </>
 }
 
